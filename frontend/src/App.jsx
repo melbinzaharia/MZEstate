9e69142ea95c2caf7bc5d26a224008fa0150fe97
Melbin Zacharia <melbin.zacharia@careniketa.com>
2023-10-14 20:09:12 +0530

    header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
@@ -8,7 +9,9 @@ import SignUp from "./pages/SignUp";
 
 export default function App() {
   return (
+   
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
