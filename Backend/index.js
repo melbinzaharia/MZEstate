4e47283af0b46d92d2aa1fafeb943ff97aa68500
Melbin Zacharia <melbin.zacharia@careniketa.com>
2023-10-16 15:57:50 +0530

    ll

--- Backend/index.js
+++ Backend/index.js
@@ -3,14 +3,14 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config()
 
+//connecting server
 const app = express();
-""
 app.listen(3000,() => {
     console.log("Server Is running on port 3000!!")
 })
 
 //connecting database
-mongoose.connect(process.env.MONGO).then(()=>{
+mongoose.connect("mongodb+srv://melbinzacharia:Melbin24.!@cluster0.zsftp0a.mongodb.net/?retryWrites=true&w=majority").then(()=>{
     console.log("CONNECTED DATABASE");
 }).catch((err)=>{
     console.log(err);
