47e09a06e5d6c88b3461abc615d82349dd106e14
Melbin Zacharia <melbin.zacharia@careniketa.com>
2023-10-16 15:53:36 +0530

    connection backend and database

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config()
 
 const app = express();
-
+""
 app.listen(3000,() => {
     console.log("Server Is running on port 3000!!")
+})
+
+//connecting database
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("CONNECTED DATABASE");
+}).catch((err)=>{
+    console.log(err);
 })
