4b667224c59da350ae8f3de3cad2c48809b0ddad
Melbin Zacharia <melbin.zacharia@careniketa.com>
2023-10-19 19:16:51 +0530

    complete signuin

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,45 +1,41 @@
-import User from "../models/user.js"
+import User from "../models/user.js";
 import bcrypt from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
 import dotenv from "dotenv";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 dotenv.config();
 
+export const auth = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json("User Created successfully");
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const auth = async (req, res ,next) => {
-    const {username , email , password}= req.body;
-    const hashedPassword = bcrypt.hashSync(password,10)
-    const newUser = new User({ username, email, password:hashedPassword});
-    try {
-        await newUser.save();
-        res.status(201).json("User Created successfully");
-    } catch(error) {
-        next(error);
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User Not found!"));
     }
-  };
-
-
-export const signin = async (req,res, next) => {
-    const {email, password} = req.body;
-
-    try {
-        const validUser = await User.findOne({email})
-        if (!validUser) {
-            return next(errorHandler(404, "User Not found!"));
-          }
-          const validPassword = bcrypt.compareSync(password, validUser?.password); 
-          if (!validPassword) {
-            return next(errorHandler(404, "User password incorrect"));
-          }
-        const token = jwt.sign({ id:validUser._id},process.env.JWT_SECRET)
-        const { password: pass, ...rest } = validUser._doc;// TO RETURN ALL THE THINGS EXCEPT THE PASSWORD
-        res
-        .cookie("access_token", token, {httpOnly:true})
-        .status(200)
-        .json(rest)
-        
-    } catch (error) {
-        next(error)
+    const validPassword = bcrypt.compareSync(password, validUser?.password);
+    if (!validPassword) {
+      return next(errorHandler(404, "User password incorrect"));
     }
-
-}
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc; // TO RETURN ALL THE THINGS EXCEPT THE PASSWORD
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
