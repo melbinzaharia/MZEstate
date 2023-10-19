a489da000353432ea70b90846b5a14f1e1439a15
Melbin Zacharia <melbin.zacharia@careniketa.com>
2023-10-19 18:43:00 +0530

    complete signuin

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,6 +1,9 @@
 import User from "../models/user.js"
 import bcrypt from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import dotenv from "dotenv";
+import jwt from 'jsonwebtoken';
+dotenv.config();
 
 
 export const auth = async (req, res ,next) => {
@@ -14,3 +17,29 @@ export const auth = async (req, res ,next) => {
         next(error);
     }
   };
+
+
+export const signin = async (req,res, next) => {
+    const {email, password} = req.body;
+
+    try {
+        const validUser = await User.findOne({email})
+        if (!validUser) {
+            return next(errorHandler(404, "User Not found!"));
+          }
+          const validPassword = bcrypt.compareSync(password, validUser?.password); 
+          if (!validPassword) {
+            return next(errorHandler(404, "User password incorrect"));
+          }
+        const token = jwt.sign({ id:validUser._id},process.env.JWT_SECRET)
+        const { password: pass, ...rest } = validUser._doc;// TO RETURN ALL THE THINGS EXCEPT THE PASSWORD
+        res
+        .cookie("access_token", token, {httpOnly:true})
+        .status(200)
+        .json(rest)
+        
+    } catch (error) {
+        next(error)
+    }
+
+}
--- Backend/routes/auth.route.js
+++ Backend/routes/auth.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { auth } from "../controllers/auth.controller.js";
+import { auth, signin } from "../controllers/auth.controller.js";
 const authRouter = express.Router();
 
 
 authRouter.post('/signup', auth);
+authRouter.post("/signin",signin);
 
 
 export default authRouter;
