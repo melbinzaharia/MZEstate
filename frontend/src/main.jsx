3165912e5890ecf81720d60febedf2afa5fffc8a
Melbin Zacharia <melbin.zacharia@careniketa.com>
2023-10-22 03:03:47 +0530

    done

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
@@ -6,14 +5,6 @@ import './index.css';
 import { persistor, store } from './redux/store.js';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-=======
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { persistor, store } from './redux/store.js'
-import { Provider } from 'react-redux'
-import './index.css'
-import { PersistGate } from 'redux-persist/integration/react'
->>>>>>> 0686cd72b435f4e74692ec766cc091a2e1cf0232
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
@@ -21,10 +12,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <App />
     </PersistGate>
   </Provider>
-<<<<<<< HEAD
 );
-=======
-
-
-)
->>>>>>> 0686cd72b435f4e74692ec766cc091a2e1cf0232
