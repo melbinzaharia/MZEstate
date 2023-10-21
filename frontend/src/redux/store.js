4af96017d3e0f97e86a2c2232b8f5105a5c284bf
Melbin Zacharia <melbin.zacharia@careniketa.com>
2023-10-21 13:56:35 +0530

    added persitence to redux

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,10 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice';
+import persistReducer from 'redux-persist/es/persistReducer';
+import persistStore from 'redux-persist/es/persistStore';
+import storage from 'redux-persist/lib/storage';
 
-export const store = configureStore({
-  reducer: { user: userReducer },
+const rootReducer = combineReducers({user: userReducer}); // to combine reducers together into one reducer
+
+const persistConfig ={ // to store configuration of persitor reducer
+    key: 'root',
+    storage,
+    version: 1,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)// to wrap root reducer into persisted state so that we  can implement persistence
+
+
+
+export const store = configureStore({  // to store and mange reducers
+  reducer:  persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: false,
+      serializableCheck: false,  // to avoid serializing error in browser
     }),
 });
+
+export const persistor = persistStore(store);
--- frontend/src/redux/user/userSlice.js
+++ frontend/src/redux/user/userSlice.js
@@ -14,7 +14,7 @@ const userSlice = createSlice({
       state.loading = true;
     },
     signInSuccess: (state, action) => {
-      (state.currentUser = action.payload), 
+      state.currentUser = action.payload, 
       state.error = null,
       state.loading = false;
     },
